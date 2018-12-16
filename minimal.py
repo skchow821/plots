0435215dc573eb989624044101963ad25bfd6b94
Sunny Chow <skchow@gmail.com>
2018-12-15 21:19:26 -0800

    Added a simple ArgumentParser

--- minimal.py
+++ minimal.py
@@ -6,7 +6,7 @@ def parse_arguments():
     parser.add_argument('-n', '--opt_number', dest='opt_number', action='store_const', const=10, default=5, help='help me')
     parser.add_argument('--opt_bool', dest='opt_bool', action='store_true', required=True)
     parser.add_argument('--count_lvl', dest='count_lvl', action='count')
-    parser.add_argument('--create_list', dest='create_list', nargs=3)
+    parser.add_argument('--create_list', dest='create_list', nargs=3, metavar='list_item')
     return parser.parse_args()
 
 def main():
