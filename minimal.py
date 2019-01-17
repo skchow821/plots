161a41a4ffc88bb395f2b94c62658c3e96d4cc4d
Sunny Chow <sunny_chow@acm.org>
2019-01-16 23:38:22 -0800

    Made skeleton main program consistent with style.

--- minimal.py
+++ minimal.py
@@ -1,22 +1,36 @@
-import sys
+""" Skeleton main + project to build from.  Consist of a logger and an
+ argument parser.
+"""
 import argparse
 import logging
 
 def parse_arguments():
+    """ Sample to parse arguments.
+    """
     parser = argparse.ArgumentParser(description='What am I?')
-    parser.add_argument('-n', '--opt_number', dest='opt_number', action='store_const', const=10, default=5, help='help me')
-    parser.add_argument('--opt_bool', dest='opt_bool', action='store_true', required=True)
+    parser.add_argument('-n', '--opt_number', dest='opt_number',
+                        action='store_const', const=10, default=5,
+                        help='help me')
+    parser.add_argument('--opt_bool', dest='opt_bool', action='store_true',
+                        required=True)
     parser.add_argument('--count_lvl', dest='count_lvl', action='count')
-    parser.add_argument('--create_list', dest='create_list', nargs=3, metavar='list_item')
+    parser.add_argument('--create_list', dest='create_list', nargs=3,
+                        metavar='list_item')
     return parser.parse_args()
 
 def setup_logging():
-    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s, %(module)s, %(funcName)s, %(message)s")
-
+    """ Sample to set up a configurable logger
+    """
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s, %(module)s,%(funcName)s,"\
+                               "%(message)s")
 def main():
+    """ Sample main program
+    """
     setup_logging()
     logging.debug('Debug Log')
     args = parse_arguments()
+    logging.debug(args)
 
 if __name__ == '__main__':
     main()
